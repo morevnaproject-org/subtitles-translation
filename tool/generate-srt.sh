a99d9502403b650e23a0159645da2249389aa8f7
Konstantin Dmitriev <ksee.zelgadis@gmail.com>
2017-08-15 12:55:30 +0700

    Move scripts to "tool" subdirectory

--- tool/generate-srt.sh
+++ tool/generate-srt.sh
@@ -4,24 +4,25 @@ set -e
 
 # TODO: Ideally, it would be nice to improve msrt_tool.py, so it can automatically extract all available languages from msrt file.
 
-BASE_DIR=$(cd `dirname "$0"`; pwd)
+SCRIPT_DIR=$(cd `dirname "$0"`; pwd)
+BASE_DIR=`dirname "${SCRIPT_DIR}"`
 
 cd ${BASE_DIR}
 
 FILE="pepper-and-carrot-ep6"
 for LANG in rus dan eng epo jbo fra deu por ita spa; do
 [ -d "${BASE_DIR}/output/${FILE}" ] || mkdir -p "${BASE_DIR}/output/${FILE}"
-python ./msrt_tool.py ${FILE}.msrt ${LANG} -o ${BASE_DIR}/output/${FILE}/${FILE}-${LANG}.srt
+python ${SCRIPT_DIR}/msrt_tool.py ${FILE}.msrt ${LANG} -o ${BASE_DIR}/output/${FILE}/${FILE}-${LANG}.srt
 done
 
 FILE="morevna-ep3"
 for LANG in rus eng; do
 [ -d "${BASE_DIR}/output/${FILE}" ] || mkdir -p "${BASE_DIR}/output/${FILE}"
-python ./msrt_tool.py ${FILE}.msrt ${LANG} -o ${BASE_DIR}/output/${FILE}/${FILE}-${LANG}.srt
+python ${SCRIPT_DIR}/msrt_tool.py ${FILE}.msrt ${LANG} -o ${BASE_DIR}/output/${FILE}/${FILE}-${LANG}.srt
 done
 
 #FILE="synfig-course-promo"
 #for LANG in rus eng epo spa; do
 #[ -d "${BASE_DIR}/output/${FILE}" ] || mkdir -p "${BASE_DIR}/output/${FILE}"
-#python ./msrt_tool.py ${FILE}.msrt ${LANG} -o ${BASE_DIR}/output/${FILE}/${FILE}-${LANG}.srt
+#python ${SCRIPT_DIR}/msrt_tool.py ${FILE}.msrt ${LANG} -o ${BASE_DIR}/output/${FILE}/${FILE}-${LANG}.srt
 #done
