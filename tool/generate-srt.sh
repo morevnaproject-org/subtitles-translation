c79a95cb6fc274838c634830055fa5e32a1c159c
scribblemaniac <scribblemaniac@users.noreply.github.com>
2017-09-01 19:27:53 -0600

    Automatically generate all languages in generate-srt

--- tool/generate-srt.sh
+++ tool/generate-srt.sh
@@ -2,23 +2,40 @@
 
 set -e
 
-# TODO: Ideally, it would be nice to improve msrt_tool.py, so it can automatically extract all available languages from msrt file.
-
 SCRIPT_DIR=$(cd `dirname "$0"`; pwd)
 BASE_DIR=`dirname "${SCRIPT_DIR}"`
 
 cd "${BASE_DIR}"
 
-FILE="pepper-and-carrot-ep6"
-for LANG in rus dan eng epo jbo fra deu por ita spa; do
-[ -d "${BASE_DIR}/output/${FILE}" ] || mkdir -p "${BASE_DIR}/output/${FILE}"
-python "${SCRIPT_DIR}/msrt_tool.py" "${FILE}.msrt" --extract "${LANG}" "${BASE_DIR}/output/${FILE}/${FILE}-${LANG}.srt"
-done
+# Extracts all languages, or only select ones if arguments are passed in
+extractLanguages () {
+  # Get language list
+  local langs file="$1"
+  shift
+  if [ $# -gt 0 ]; then
+    # Languages were specified in positional arguments, use them
+    langs=$@
+  else
+    # No languages specified, get all of them
+    langs=$("${SCRIPT_DIR}/msrt_tool.py" --list "${BASE_DIR}/$file.msrt" | cut -d ' ' -f1)
+  fi
 
-FILE="morevna-ep3"
-for LANG in rus eng; do
-[ -d "${BASE_DIR}/output/${FILE}" ] || mkdir -p "${BASE_DIR}/output/${FILE}"
-python "${SCRIPT_DIR}/msrt_tool.py" "${FILE}.msrt" --extract "${LANG}" "${BASE_DIR}/output/${FILE}/${FILE}-${LANG}.srt"
+  # Extract languages from list 1 at a time
+  local lang
+  for lang in ${langs}; do
+    #if [[ -n $lang ]]; then
+      echo "Generating $lang for $file"
+      [ -d "${BASE_DIR}/output/$file" ] || mkdir -p "${BASE_DIR}/output/$file"
+      echo "$lang" "${BASE_DIR}/output/$file/$file-$lang.srt" "${BASE_DIR}/$file.msrt"
+      "${SCRIPT_DIR}/msrt_tool.py" --extract "$lang" "${BASE_DIR}/output/$file/$file-$lang.srt" "${BASE_DIR}/$file.msrt"
+    #fi
+  done
+}
+
+FILES=("pepper-and-carrot-ep6" "morevna-ep3")
+
+for FILE in "${FILES[@]}"; do
+  extractLanguages "$FILE"
 done
 
 #FILE="synfig-course-promo"
