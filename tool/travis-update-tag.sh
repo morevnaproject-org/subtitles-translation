6779c1f7b0a467d7aa0cfdfcff2befde153d73a2
Konstantin Dmitriev <ksee.zelgadis@gmail.com>
2018-04-03 15:47:11 +0700

    Deploy test #11

---
+++ tool/travis-update-tag.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+
+cd "${TRAVIS_BUILD_DIR}"
+
+git config --global push.default simple
+git config user.name "Travis CI"
+git config user.email "travis@travis-ci.org"
+
+git checkout "${TRAVIS_BRANCH}"
+git push origin :refs/tags/latest-release
+git tag -d latest-release || true
+git tag latest-release
+git push "https://${GH_REPO_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" --tags
