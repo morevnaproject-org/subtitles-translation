40840dd4499b50c356ade82bda24bf739b78556f
Konstantin Dmitriev <ksee.zelgadis@gmail.com>
2018-04-03 16:17:47 +0700

    Deploy test #16

--- tool/deploy-srt.sh
+++ tool/deploy-srt.sh
@@ -14,6 +14,3 @@ git commit -m "Extract srt files for commit: ${TRAVIS_COMMIT}" -m "Travis build:
 git push "https://${GH_REPO_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" "${TRAVIS_BRANCH}"
 
 git push "https://${GH_REPO_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" :refs/tags/latest-release
-git tag -d latest-release || true
-git tag latest-release
-git push "https://${GH_REPO_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" --tags
