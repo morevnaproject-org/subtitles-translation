0367a6d28319cc66096061f9fb770966d57ba317
Konstantin Dmitriev <ksee.zelgadis@gmail.com>
2018-04-03 18:04:43 +0700

    Deploy test #20

--- tool/deploy-srt.sh
+++ tool/deploy-srt.sh
@@ -12,3 +12,10 @@ git checkout "${TRAVIS_BRANCH}"
 git add --all
 git commit -m "Extract srt files for commit: ${TRAVIS_COMMIT}" -m "Travis build: ${TRAVIS_BUILD_NUMBER} [ci skip]"
 git push "https://${GH_REPO_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" "${TRAVIS_BRANCH}"
+
+echo "Pushing tags..."
+git push "https://${GH_REPO_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" :refs/tags/latest-release
+git tag -d latest-release || true
+git tag latest-release
+git push "https://${GH_REPO_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" --tags
+echo "Pushing tags done."
