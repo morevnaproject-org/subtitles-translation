05f0552dbe9f1077c35cad51c314c389481b6008
Konstantin Dmitriev <ksee.zelgadis@gmail.com>
2018-04-03 22:15:58 +0700

    Travis: Change release tag name. Do not trigger deployment if no changes were made to output files.

--- tool/deploy-srt.sh
+++ tool/deploy-srt.sh
@@ -10,12 +10,12 @@ git config user.email "travis@travis-ci.org"
 
 git checkout "${TRAVIS_BRANCH}"
 git add --all
-git commit -m "Extract srt files for commit: ${TRAVIS_COMMIT}" -m "Travis build: ${TRAVIS_BUILD_NUMBER} [ci skip]" || true
-git push "https://${GH_REPO_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" "${TRAVIS_BRANCH}" || true
+git commit -m "Extract srt files for commit: ${TRAVIS_COMMIT}" -m "Travis build: ${TRAVIS_BUILD_NUMBER} [ci skip]"
+git push "https://${GH_REPO_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" "${TRAVIS_BRANCH}"
 
 echo "Pushing tags..."
-git push "https://${GH_REPO_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" :refs/tags/latest-release
-git tag -d latest-release || true
-git tag latest-release
+git push "https://${GH_REPO_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" :refs/tags/current
+git tag -d current || true
+git tag current
 git push "https://${GH_REPO_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" --tags
 echo "Pushing tags done."
